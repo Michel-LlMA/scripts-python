97b55cbfe8e62c1b7bbe2556b06b36a28425c82b
Michel Lima <m1chel.lima@hotmail.com>
2023-03-30 16:34:33 -0300

    Desafios

--- Desafio 22.py
+++ Desafio 22.py
@@ -1,6 +1,7 @@
-nome = str(input('Qual é seu nome completo? '))
+nome = str(input('Qual é seu nome completo? ')).strip()
 print ('Seu nome com todas letras maiúsculas é: {}'.format(nome.upper()))
 print ('Seu nome com todas letras minúsculas é: {}'.format(nome.lower()))
 print ('Seu nome completo tem um total de {} letras'.format(len(nome.replace(' ', ''))))
 pnome = nome.split()[0]
 print ('Seu primeiro nome tem {} letras.'.format(len(pnome)))
+#print ('Seu primeiro nome tem {} letras'.format(nome.find(' ')))
---
+++ Desafio 25.py
@@ -0,0 +1,2 @@
+nome = str(input('Qual é seu nome? '))
+print ('Seu nome tem Garcia? {}'.format('GARCIA' in nome.upper()))
